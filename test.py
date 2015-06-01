869fc7301be744e218f39eba4cf7809cadf3ac79
YanshuoH <hys235606751@gmail.com>
2015-06-01 15:03:08 +0200

    remove python testing files

--- test.py
+++
@@ -1,7 +0,0 @@
-from lib.APIClient import APIClient
-
-apiClient = APIClient(client_id='ce4eda46cdc144cb85e747654cc5cd25',
-                      app_secret='b97cfad9ef8c4403b04ff23b82d5e1fe',
-                      redirect_uri='')
-
-print apiClient.get_request_url()
